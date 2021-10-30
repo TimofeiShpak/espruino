8f7949976e1085eb542a26edf7e98589d720975d
TimofeiShpak <tima.shpak.v@gmail.com>
2021-10-30 21:12:06 +0400

    add news

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
-const http = require("http");
-const fs = require("fs");
+// const http = require("http");
+// const fs = require("fs");
   
-http.createServer(function(request, response){
+// http.createServer(function(request, response){
       
-    console.log(`Запрошенный адрес: ${request.url}`);
-    // получаем путь после слеша
-    const filePath = request.url.substr(1);
-    fs.readFile(filePath, function(error, data){
+//     console.log(`Запрошенный адрес: ${request.url}`);
+//     // получаем путь после слеша
+//     const filePath = request.url.substr(1);
+//     fs.readFile(filePath, function(error, data){
               
-        if(error){
+//         if(error){
                   
-            response.statusCode = 404;
-            response.end("Resourse not found!");
-        }   
-        else{
-          handlePOST(request)
-            response.end(data);
-        }
-    });
-}).listen(3000, function(){
-    console.log("Server started at 3000");
-});
+//             response.statusCode = 404;
+//             response.end("Resourse not found!");
+//         }   
+//         else{
+//           handlePOST(request)
+//             response.end(data);
+//         }
+//     });
+// }).listen(3000, function(){
+//     console.log("Server started at 3000");
+// });
 
-function handlePOST(req) {
-  var data = "";
-  req.on('data', function(d) { data += d; console.log(data) });
-  req.on('end', function() {
-    postData = {};
-    data.split("&").forEach(function(el) {
-      var els = el.split("=");
-      postData[els[0]] = decodeURIComponent(els[1]);
-    });
-    // console.log('code-1', postData.code);
-    // eval(postData.code)
-  });
-}
+// function handlePOST(req) {
+//   var data = "";
+//   req.on('data', function(d) { data += d; console.log(data) });
+//   req.on('end', function() {
+//     postData = {};
+//     data.split("&").forEach(function(el) {
+//       var els = el.split("=");
+//       postData[els[0]] = decodeURIComponent(els[1]);
+//     });
+//     // console.log('code-1', postData.code);
+//     // eval(postData.code)
+//   });
+// }
+
+const http = require("http");
+ 
+http.createServer(function(request, response){
+     
+    response.end("Hello world!");
+}).listen(3000);
